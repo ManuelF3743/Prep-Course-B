3230646e11fdce25f96dcb04bfbf032aafa3b923
ManuelF3743 <maximiun.mra@hotmail.com>
2022-11-01 10:35:10 -0600

    Tarea lista

--- 03-JS-III/homework/homework.js
+++ 03-JS-III/homework/homework.js
@@ -8,29 +8,40 @@ function imprimirSumaNumeros() {
     // Imprime la suma de los números del 1 al 10
     // Pista: usa un acumulador
     // Tu código:
+    var sum=0;
+    for(var i=1;i<=10;i++){
+      sum=sum+i;
+    }
+    return sum;
 }
 
 function encuentraPares(array){
   // Devuelve un arreglo con los pares encontrados
   // en el arreglo de enteros pasado como parámetro
   // Tu código:
+  return array.filter((e) => e%2===0);
 }
 
 function elevaAlCuadrado(array){
   // Devuelve un arreglo con cada número del array
   // elevado al cuadrado
   // Tu código:
+  return array.map(current=>current**2);
 }
 
 function sumaArray(array){
   // Devuelve el resultado de sumar todos los elementos
   // de un arreglo de enteros dado
   // Tu código:
+  const initialValue=0;
+  return array.reduce((previusValue, currentValue)=>previusValue + currentValue, initialValue);
 }
 
 function numeroDigitos(num){
   // Devuelve el número de dígitos de un número dado
   // Tu código:
+  num=num+"";
+  return num.length;
 }
   
   
